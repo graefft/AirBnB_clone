43a3a9b1014c573988c8df4c91c7352b299b2895
David Kwan <dwkwan1989@gmail.com>
2019-07-03 18:06:39 +0000

    Setup and TearDown. Create new FS instance for each test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,36 +13,37 @@ class TestFileStorageClass(unittest.TestCase):
 
     def setUp(self):
         """Defines instructions that will be executed before each test"""
-        pass
+        if os.path.exists("file.json"):
+            os.remove("file.json")
 
     def tearDown(self):
         """Defines instructions that will be executed after each test"""
         if os.path.exists("file.json"):
             os.remove("file.json")
-        models.storage.reset()
 
     def test_instancecreation(self):
         """Test that instance of FileStorage is properly created"""
         Storage = FileStorage()
         self.assertTrue(type(Storage) == FileStorage)
         self.assertTrue(isinstance(Storage, FileStorage))
-        Storage.reset()
 
     def test_privateclassvariableStorage(self):
         """Test that instance of FileStorage is properly created"""
         Storage = FileStorage()
         with self.assertRaises(AttributeError) as e:
-            self.assertEqual(str(e.exception),
-                             "'FileStorage' object has no" +
-                             " attribute 'objects'")
+            print(Storage.__objects)
+        self.assertEqual(str(e.exception),
+                         "'FileStorage' object has no" +
+                         " attribute '_TestFileStorageClass__objects'")
 
     def test_privateclassvariablefilepath(self):
         """Test that instance of FileStorage is properly created"""
         Storage = FileStorage()
         with self.assertRaises(AttributeError) as e:
-            self.assertEqual(str(e.exception),
-                             "'FileStorage' object has no" +
-                             " attribute 'file_path'")
+            print(Storage.__file_path)
+        self.assertEqual(str(e.exception),
+                         "'FileStorage' object has no" +
+                         " attribute '_TestFileStorageClass__file_path'")
 
     def test_attributes(self):
         '''Tests storage for attributes'''
@@ -51,11 +52,12 @@ class TestFileStorageClass(unittest.TestCase):
         self.assertTrue(hasattr(FileStorage, "_FileStorage__file_path"))
         self.assertTrue(hasattr(FileStorage, "_FileStorage__objects"))
 
-    def test_storage_id(self):
-        '''Tests id for Storage'''
-        b1 = BaseModel()
-        self.assertTrue(hasattr(b1, "id"))
-        self.assertEqual(type(b1.id), str)
+    def test_instanceattributes(self):
+        '''Tests storage for attributes'''
+        Storage = FileStorage()
+        Storage.reset()
+        self.assertTrue(hasattr(Storage, "_FileStorage__file_path"))
+        self.assertTrue(hasattr(Storage, "_FileStorage__objects"))
 
     def test_storage_all_return(self):
         '''Tests that all returns dict'''
@@ -64,12 +66,16 @@ class TestFileStorageClass(unittest.TestCase):
 
     def test_all(self):
         """Tests the all method of File Storage class"""
+        Storage = FileStorage()
         b1 = BaseModel()
         b2 = BaseModel()
         b3 = BaseModel()
-        self.assertFalse(models.storage.all() == {})
-        objdict = models.storage.all()
+        Storage.new(b1)
+        Storage.new(b2)
+        Storage.new(b3)
+        objdict = Storage.all()
         self.assertEqual(type(objdict), dict)
+        self.assertFalse(objdict == {})
         self.assertTrue("BaseModel.{}".format(b1.id) in objdict)
         self.assertTrue("BaseModel.{}".format(b2.id) in objdict)
         self.assertTrue("BaseModel.{}".format(b3.id) in objdict)
@@ -128,7 +134,6 @@ class TestFileStorageClass(unittest.TestCase):
     def test_reloadbyclearingdictionary(self):
         """Tests the reload method of File Storage class"""
         Storage = FileStorage()
-        Storage.reset()
         b1 = BaseModel()
         Storage.new(b1)
         olddict = Storage.all()
@@ -136,23 +141,32 @@ class TestFileStorageClass(unittest.TestCase):
         Storage.reset()
         Storage.reload()
         newdict = Storage.all()
-        olddict = models.storage.all()
-        models.storage.save()
-        models.storage.reset()
-        models.storage.reload()
-        newdict = models.storage.all()
         for key, value in olddict.items():
             self.assertTrue(key in newdict)
 
-    def reload_2(self):
+    def test_reload_2(self):
         '''Tests reload normal'''
         Storage = FileStorage()
-        b1 = BaseMode()
+        b1 = BaseModel()
         Storage.new(b1)
         Storage.save()
         Storage.reset()
         Storage.reload()
-        self.assertTrue(Storage.all()[BaseModel.b.id])
+        self.assertTrue(Storage.all()["BaseModel.{}".format(b1.id)])
+        self.assertTrue(Storage._FileStorage__objects["BaseModel.{}".
+                                                      format(b1.id)])
+
+    def test_reload(self):
+        '''Tests reload'''
+        Storage = FileStorage()
+        b1 = BaseModel()
+        b2 = BaseModel()
+        b3 = BaseModel()
+        objdict = Storage.all()
+        b1.save()
+        Storage.reload()
+        objdict_resave = Storage.all()
+        self.assertTrue(objdict == objdict_resave)
 
 if __name__ == "__main__":
     unittest.main()
